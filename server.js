70e0be41f72aedabe34a007d48e61ea1a698933d
Jeremias Ehnnle <jeremias.ehnle@aol.com>
2020-11-28 17:01:43 +0100

    Fix error with server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const app = require('express') ();
+const http = require('http').createServer (app);
+const io = require('socket.io') (http);
 const Map = require ("./models/Map");
 const Player = require ("./models/Player");
 
@@ -11,65 +12,65 @@ const players = [];
 
 // Send game-data to all players in a specific interval
 setInterval (() => {
-    io.sockets.emit('map-updated', map.State);
+    io.sockets.emit ('map-updated', map.State);
 }, 15000);
 
 io.on("connection", (socket) => {
-    console.log('a user connected');
+    console.log ('a user connected');
 
     // Update player-data
     socket.on("update-color", (data) => {
-        console.log("update-color", data);
+        console.log ("update-color", data);
         player.changeColor (data.color);
         // Send name-change to everyone
-        io.sockets.emit('player-updated', player.State);
+        io.sockets.emit ('player-updated', player.State);
     });
     socket.on("update-name", (data) => {
-        console.log("update-name", data);
+        console.log ("update-name", data);
         player.changeName (data.name);
         // Send name-change to everyone
-        io.sockets.emit('player-updated', player.State);
+        io.sockets.emit ('player-updated', player.State);
     });
 
     // Enter/Leave game
     const playerLeaved = () => {
         map.removePlayer (player);
-        io.sockets.emit('player-leaved', player.State);
+        io.sockets.emit ('player-leaved', player.State);
     }
     socket.on("disconnect", () => {
-        console.log('user disconnected');
+        console.log ('user disconnected');
         playerLeaved ();
     });
     socket.on ("leave-game", (data) => {
-        console.log("leave-game", data);
+        console.log ("leave-game", data);
         playerLeaved ();
     });
     socket.on ("start-game", (data) => {
-        console.log("start-game", data);
+        console.log ("start-game", data);
         map.addPlayer (player);
 
         // Send map-data to the player
         socket.emit ("map-updated", map.State);
         // Update player-position and co.
-        io.sockets.emit('player-updated', player.State);
+        io.sockets.emit ('player-updated', player.State);
     });
 
     // Game states
     // - move
     socket.on ("move-player", (data) => {
         player.move (data.x, data.y);
-        io.sockets.emit('player-updated', player.State);
+        io.sockets.emit ('player-updated', player.State);
     });
 
     // - rotate
     socket.on ("rotate-player", (data) => {
         player.move (data.rotation);
-        io.sockets.emit('player-updated', player.State);
+        io.sockets.emit ('player-updated', player.State);
     });
 
     // - shrink / grow
     const sizeEvent = () => {
-        io.sockets.emit('player-updated', player.State);
+        io.sockets.emit ('player-updated', player.State);
     };
 
     // Add new player to server
